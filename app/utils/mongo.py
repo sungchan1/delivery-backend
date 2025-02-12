ce14ef0d1cd43c683189404f8b8c9a86d57af264
sungchan <sungchan1@naver.com>
2025-02-12 18:00:36 +0900

    Implement MongoDB connection setup

--- app/utils/mongo.py
+++ app/utils/mongo.py
@@ -6,3 +6,10 @@ DATABASE_NAME = os.environ.get("MONGO_DATABASE", "yorigin")
 
 client = AsyncIOMotorClient()
 db = client[DATABASE_NAME]
+
+# # 버전 정보 가져오기
+# import asyncio
+# async def print_mongo_version() -> None:
+#     status = await client.test.command("serverStatus")
+#     print(status["version"])
+#     asyncio.run(print_mongo_version())
