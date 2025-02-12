5cf3d2aab13413d1644d4924a8522e2326bc1097
sungchan <sungchan1@naver.com>
2025-02-12 18:01:17 +0900

    Implement mongodb index

--- app/main.py
+++ app/main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
 
+from app.apis.category.v1.home_category_router import router as home_category_router
+from app.entities.collections import set_indexes
+
 app = FastAPI()
+app.include_router(home_category_router)
+
+
+@app.on_event("startup")
+async def on_startup() -> None:
+    await set_indexes()
