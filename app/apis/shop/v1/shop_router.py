69a164af338bd597128b54b1ee8d47f75b500c6b
sungchan1 <sungchan254@naver.com>
2025-02-13 15:33:26 +0900

    Implement delete shop router & test

--- app/apis/shop/v1/shop_router.py
+++ app/apis/shop/v1/shop_router.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter
+from bson import ObjectId
+from bson.errors import InvalidId
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import ORJSONResponse
+from starlette import status
+from starlette.responses import Response
 
+from app import ShopNotFoundException
 from app.dtos.shop.shop_creation_response import ShopCreationResponse
-from app.services.shop_service import ShopCreationRequest, create_shop
+from app.services.shop_service import ShopCreationRequest, create_shop, delete_shop
 
 router = APIRouter(prefix="/v1/shops", tags=["Shop"], redirect_slashes=False)
 
@@ -15,3 +20,21 @@ router = APIRouter(prefix="/v1/shops", tags=["Shop"], redirect_slashes=False)
 async def api_create_shop(shop_creation_request: ShopCreationRequest) -> ShopCreationResponse:
     shop = await create_shop(shop_creation_request)
     return ShopCreationResponse(id=str(shop.id))
+
+
+@router.delete(
+    "/{shop_id}", description="shop 을 삭제합니다.", response_class=Response, status_code=status.HTTP_204_NO_CONTENT
+)
+async def api_delete_shop(shop_id: str) -> None:
+    try:
+        await delete_shop(ObjectId(shop_id))
+    except ShopNotFoundException as e:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail={"message": e.response_message},
+        )
+    except InvalidId:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail={"message": "id should be valid bson object id"},
+        )
--- app/tests/apis/shop/v1/test_shop_router.py
+++ app/tests/apis/shop/v1/test_shop_router.py
@@ -4,6 +4,8 @@ from httpx import AsyncClient
 
 from app.entities.category.category_codes import CategoryCode
 from app.entities.collections import ShopCollection
+from app.entities.collections.geo_json import GeoJsonPolygon
+from app.entities.collections.shop.shop_document import ShopDeliveryAreaSubDocument
 from app.main import app
 
 
@@ -30,3 +32,51 @@ async def test_api_create_shop() -> None:
     assert shop["category_codes"] == request_body["category_codes"]
     assert len(shop["delivery_areas"]) == 1
     assert shop["delivery_areas"][0]["poly"]["coordinates"] == coordinates
+
+
+async def test_api_delete_shop() -> None:
+    # Given
+    shop = await ShopCollection.insert_one(
+        name="sandwich_pizza_shop",
+        category_codes=[CategoryCode.SANDWICH, CategoryCode.PIZZA],
+        delivery_areas=[
+            ShopDeliveryAreaSubDocument(poly=GeoJsonPolygon(coordinates=[[[0, 0], [0, 10], [10, 10], [10, 0], [0, 0]]]))
+        ],
+    )
+    # When
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.delete(
+            f"/v1/shops/{shop.id}",
+        )
+
+    # Then
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert await ShopCollection.find_by_id(shop.id) is None
+
+
+async def test_api_delete_shop_when_delete_non_existing_shop_then_it_returns_400() -> None:
+    # Given
+    non_existing_shop_id = "64622be98d618c0d271a0238"
+
+    # When
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.delete(
+            f"/v1/shops/{non_existing_shop_id}",
+        )
+
+    # Then
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+async def test_api_delete_shop_when_delete_invalid_shop_then_it_returns_422() -> None:
+    # Given
+    invalid_shop_id = "invalid_id"
+
+    # When
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        response = await client.delete(
+            f"/v1/shops/{invalid_shop_id}",
+        )
+
+    # Then
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
